b31b5df47943b9af6da408ee5d984be533ca22bf
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2020-01-05 23:38:55 +0100

    Create ft_print_comb.c

---
+++ Day02/ft_print_comb.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c ,1);
+}
+
+void ft_print_comb(void)
+{
+    char left = '0';
+    char mid = '1';
+    char right = '2';
+    
+    for (; left < '8';) {
+        for (; mid < '9';) {
+            for (; right < ':'; right++) {
+                ft_putchar(left);
+                ft_putchar(mid);
+                ft_putchar(right);
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+            mid++;
+            right = mid + 1;
+        }
+        left++;
+        mid = left;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ft_print_comb();
+    return (0);
+}
