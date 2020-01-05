83057fa39fb9bc43365d1e71206e547ff3de3ec7
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2020-01-05 20:46:39 +0100

    Create ft_strrev.c

---
+++ Day03/ft_strrev.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int index = 0;
+    
+    for (; str[index] != '\0'; index++);
+    return (index);
+}
+
+char *ft_strrev(char *str)
+{
+    int i = 0;
+    int j = ft_strlen(str) - 1;
+    char save = 0;
+    
+    for (; i < j; i++) {
+        save = str[i];
+        str[i] = str[j];
+        str[j] = save;
+        j--;
+    }
+    return (str);
+}
+
+int main(int argc, char **argv)
+{
+    char str[] = "123456789";
+
+    printf("Avant fonction : %s\n", str);
+    ft_strrev(str);
+    printf("Apres fonction : %s\n", str);
+}
