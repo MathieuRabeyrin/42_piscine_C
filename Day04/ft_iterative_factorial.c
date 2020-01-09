43529e2a55b484360f51ba10c9cf1e5954d2850e
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2020-01-09 13:12:19 +0100

    Update ft_iterative_factorial.c

--- Day04/ft_iterative_factorial.c
+++ Day04/ft_iterative_factorial.c
@@ -15,7 +15,7 @@ int ft_iterative_factorial(int nb)
  
 int main(int argc, char **argv)
 {
-    for (int nb; nb < 11; nb++)
+    for (int nb = 0; nb < 11; nb++)
         printf("factorial of %d: %d\n", nb, ft_iterative_factorial(nb));
     return (0);
 }
