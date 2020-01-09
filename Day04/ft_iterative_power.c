b70b292676c18f37e2c471c9711e015f1de1645f
Mathieu Rabeyrin <m.rabeyrin0@gmail.com>
2020-01-09 13:44:37 +0100

    Update ft_iterative_power.c

--- Day04/ft_iterative_power.c
+++ Day04/ft_iterative_power.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
- 
+
 int ft_iterative_power(int nb, int power)
 {
     const int save = nb;
     int negative = 1;
-    
+
     if (nb < 0) {
         nb = -nb;
         negative = -1;
     }
+    if (power < 0)
+        return (0);
     if (power == 0)
         return (1 * negative);
     for (; power > 1; power--)
@@ -18,6 +20,6 @@ int ft_iterative_power(int nb, int power)
  
 int main(int argc, char **argv)
 {
-    printf("%d\n", ft_iterative_power(2, 3));
+    printf("%d\n", ft_iterative_power(3, 3));
     return (0);
 }
